ae0a54194f151b8e0ba52d0f69f724ca2b42546c
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-04 19:32:04 +0000

    migrated the square and the results page

--- app/controllers/omni_controller.rb
+++ app/controllers/omni_controller.rb
@@ -4,6 +4,8 @@ class OmniController < ApplicationController
   end
 
   def square_result
+    @user_input = params.fetch("square_number").to_f
+    @square = @user_input ** 2
     render(template: "templates/square_result")
   end
 
